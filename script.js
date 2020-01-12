363a61a47523a356b13fa7c2f8bc3f1c38b48b00
John Requizo <jr.jemerson14@gmail.com>
2020-01-13 10:08:40 +1100

    Loop to add 16 div rows

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const div = document.querySelectorAll("#container")
+const container = document.querySelector("#container");
+
+
+
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("row" + i);
+    div.textContent = "Row " + i;
+    container.appendChild(div);
+}
+
