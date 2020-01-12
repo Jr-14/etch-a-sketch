b24d39da0ad4d3991e65197c1ab80ee183aefb0d
John Requizo <jr.jemerson14@gmail.com>
2020-01-13 10:21:07 +1100

    created loop for a 16x16 div container

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.querySelector("#container");
 
+function createDiv () {
+    let div = document.createElement("div");
+    return div;
+}
 
 
 for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
-    div.classList.add("row" + i);
+    div.classList.add("row");
     div.textContent = "Row " + i;
     container.appendChild(div);
 }
 
+const rows = document.querySelectorAll(".row");
+
+rows.forEach((div) => {
+    for (let i = 0; i < 16; i++) {
+        div.appendChild(createDiv());
+    }
+});
