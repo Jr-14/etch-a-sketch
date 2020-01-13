a53cb35a6e22970ff050cb7f842e89bf2ae4654d
John Requizo <jr.jemerson14@gmail.com>
2020-01-13 17:15:37 +1100

    Set grid width maximum to 960px, and allowed user to set the grid Area

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var initialGridSize = 16;
+var gridExists;
+var gridSize;
+
 const container = document.querySelector("#container");
 const setGridArea = document.querySelector("#btn");
+const deleteGridArea = document.querySelector("#btn-clear");
 
-var gridSize = 16;
-
+//Creates Grid Size
 setGridArea.addEventListener('click', () => {
     let gridSize = window.prompt('Please enter grid size');
     createGrid(gridSize);
@@ -10,29 +14,44 @@ setGridArea.addEventListener('click', () => {
 
 
 function createGrid (gridArea) {
+    deleteGrid();
+    
     for (let i = 0; i < (gridArea*gridArea); i++) {
         const div = document.createElement("div");
         div.classList.add("row");
         div.textContent = "";
+        
+        container.style.display = 'grid';
+        container.style.width = '960px';
+        container.style.gridTemplateColumns = 'repeat(' + gridArea + ', 1fr)';
+        container.style.gridAutoRows = (960/gridArea)+'px';
         container.appendChild(div);
+
+        
     }
+
+    createMouseHover();
 }
 
-createGrid(16);
-//Create square grid rows from the gridSize
 
-container.style.display = 'grid';
-container.style.gridTemplateColumns = 'repeat(' + gridSize + ', 1fr)';
-container.style.gridAutoRows = '3vmax';
-
-const hover = document.querySelectorAll('.row');
-
-hover.forEach((div) => {
+function createMouseHover () {
+    const row = document.querySelectorAll('.row');
+    row.forEach((div) => {
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = 'lightblue';
-    });
-        div.addEventListener('mouseleave', () => {
-           div.style.backgroundColor = 'transparent';
         });
-});
+    });
+}
 
+function deleteGrid () {
+    const row = document.querySelectorAll('.row');
+    row.forEach((div) => {
+        container.removeChild(div);
+    });
+}
+
+
+//Delete Grid
+deleteGridArea.addEventListener('click', () => {
+    deleteGrid();
+});
